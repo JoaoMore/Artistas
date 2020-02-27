fe62579c21925330f075f2c7d2cab80f79db8c2a
Joao Moraes <joao.myname@gmail.com>
2020-02-27 17:01:39 -0300

    att

--- preprocessing.R
+++ preprocessing.R
@@ -7,10 +7,17 @@ load(file = 'data/musicas.RData')
 
 # Pré-processamento -------------------------------------------------------
 
-names(musicas)
-
+# selecionando as variaveis de interesse e removendo um artista aleatório que apareceu (Claude Debussy) (?!?!?!)
 musicas <- musicas %>% 
   select(-c(artist_id,album_id,album_type,album_images,album_release_date_precision,
            track_id, analysis_url, artists, available_markets, disc_number, 
            duration_ms, track_href, is_local, track_preview_url, type, track_uri, 
-           external_urls.spotify))
+           external_urls.spotify)) %>% 
+  filter(artist_name != 'Claude Debussy')
+
+# capitalizando os nomes das musicas para poder remover as duplicatas por album
+musicas <- musicas %>% 
+  mutate(track_name = tools::toTitleCase(track_name)) %>% 
+  distinct_at(.vars = vars(artist_name, track_name, album_name),
+              .keep_all = TRUE)#, .keep_all = TRUE)
+
