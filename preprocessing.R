f5fba2c23e3e2f92715f670ac804dff11985f2d2
Joao Moraes <joao.myname@gmail.com>
2020-02-27 15:38:30 -0300

    preprocessing

--- preprocessing.R
+++ preprocessing.R
@@ -9,7 +9,7 @@ load(file = 'data/musicas.RData')
 
 names(musicas)
 
-musicas %>% 
+musicas <- musicas %>% 
   select(-c(artist_id,album_id,album_type,album_images,album_release_date_precision,
            track_id, analysis_url, artists, available_markets, disc_number, 
            duration_ms, track_href, is_local, track_preview_url, type, track_uri, 
