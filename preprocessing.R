70a704ca9da1f7c3b9ed2e5802dc16fe8978fe7d
Joao Moraes <joao.myname@gmail.com>
2020-02-29 09:22:16 -0300

    att

--- preprocessing.R
+++ preprocessing.R
@@ -1,9 +1,12 @@
 library(tidyverse)
 
+# Definindo funções -------------------------------------------------------
+
+`%notin%` <- Negate(`%in%`)
+
 # Carregando o arquivo com as musicas -------------------------------------
 
 load(file = 'data/musicas.RData')
-
 
 # Pré-processamento -------------------------------------------------------
 
@@ -13,11 +16,14 @@ musicas <- musicas %>%
            track_id, analysis_url, artists, available_markets, disc_number, 
            duration_ms, track_href, is_local, track_preview_url, type, track_uri, 
            external_urls.spotify)) %>% 
-  filter(artist_name != 'Claude Debussy')
+  filter(artist_name %notin% c("Y'man Dog",'Claude Debussy'))
 
 # capitalizando os nomes das musicas para poder remover as duplicatas por album
 musicas <- musicas %>% 
-  mutate(track_name = tools::toTitleCase(track_name)) %>% 
+  mutate(track_name = tools::toTitleCase(track_name)) 
+
+musicas <- musicas %>% 
   distinct_at(.vars = vars(artist_name, track_name, album_name),
               .keep_all = TRUE)#, .keep_all = TRUE)
 
+save(musicas, file = 'data/musicas.RData')
