72252d7496d3443074f80334de3faee9768d79df
Joao Moraes <joao.myname@gmail.com>
2020-03-31 17:53:54 -0300

    estrstruturaçao do dataset de distancias

--- distance.R
+++ distance.R
@@ -4,27 +4,34 @@ load('data/musicas_novo.RData')
 load('data/artistas_summarized.RData')
 load('data/albuns_summarized.RData')
 
-estrangeiras <- musicas_novo %>% 
-  filter(nacionalidade == 'estrangeira') %>% 
-  .$artist_name %>% 
-  unique()
 
-brasileiras <- musicas_novo %>% 
-  filter(nacionalidade == 'brasileira') %>% 
-  .$artist_name %>% 
-  unique()
+nac <- musicas_novo %>% 
+  select(artist_name, nacionalidade) %>% 
+  distinct()
 
-# Função que verifica a nacionalidade 
+# Função que verifica se o par de artistas tem a mesma nacionalidade ou não
 compare <- function(x,y){
-  map2(x, y, ~ 1 - (.x %in% estrangeiras & .y %in% estrangeiras)) %>% unlist()
+  map2(x, y, ~ identical(.x,.y)) %>% unlist()
 }
 
 
+# funcao que retorna a artista mais próxima -------------------------------
 
-artistas_summarized %>% 
+artist.distance <- function(name, difference = FALSE, reverse = FALSE) {
+  testex %>% 
+    filter(artist_1 == name & diff == difference) %>% 
+    arrange((-1)^(TRUE)*distance) %>% 
+    .$artist_2 %>% 
+    .[1]
+}
+
+testex <- artistas_summarized %>% 
   select_if(is.numeric) %>% 
   dist() %>% 
   as.matrix(labels = TRUE) %>% 
   reshape2::melt(varnames = c('artist_1', 'artist_2'), value.name = 'distance') %>% 
   filter(distance != 0) %>% 
-  mutate(diff = compare(artist_1, artist_2))
+  left_join(., nac, by = c('artist_1' = 'artist_name')) %>% 
+  left_join(., nac, by = c('artist_2' = 'artist_name')) %>% 
+  rename(nac_1 = nacionalidade.x, nac_2 = nacionalidade.y) %>% 
+  mutate(diff = compare(nac_1,nac_2))
